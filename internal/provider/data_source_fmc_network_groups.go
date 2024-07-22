70a3047188436331bdbcf6bfc889c457cb60d6f8
Jakub Bielecki <47531708+jabielecki@users.noreply.github.com>
2024-07-22 10:20:40 +0200

    partial auto_gen base for manual work

---
+++ internal/provider/data_source_fmc_network_groups.go
@@ -0,0 +1,166 @@
+// Copyright © 2023 Cisco Systems, Inc. and its affiliates.
+// All rights reserved.
+//
+// Licensed under the Mozilla Public License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://mozilla.org/MPL/2.0/
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: MPL-2.0
+
+package provider
+
+// Section below is generated&owned by "gen/generator.go". //template:begin imports
+import (
+	"context"
+	"fmt"
+	"net/url"
+
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+	"github.com/netascode/go-fmc"
+)
+
+// End of section. //template:end imports
+
+// Section below is generated&owned by "gen/generator.go". //template:begin model
+
+// Ensure the implementation satisfies the expected interfaces.
+var (
+	_ datasource.DataSource              = &NetworkGroupsDataSource{}
+	_ datasource.DataSourceWithConfigure = &NetworkGroupsDataSource{}
+)
+
+func NewNetworkGroupsDataSource() datasource.DataSource {
+	return &NetworkGroupsDataSource{}
+}
+
+type NetworkGroupsDataSource struct {
+	client *fmc.Client
+}
+
+func (d *NetworkGroupsDataSource) Metadata(_ context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_network_groups"
+}
+
+func (d *NetworkGroupsDataSource) Schema(ctx context.Context, req datasource.SchemaRequest, resp *datasource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		// This description is used by the documentation generator and the language server.
+		MarkdownDescription: "This plural data source can query a bulk of Network Groups.",
+
+		Attributes: map[string]schema.Attribute{
+			"id": schema.StringAttribute{
+				MarkdownDescription: "The id of the object",
+				Required:            true,
+			},
+			"domain": schema.StringAttribute{
+				MarkdownDescription: "The name of the FMC domain",
+				Optional:            true,
+			},
+			"items": schema.MapNestedAttribute{
+				MarkdownDescription: "Map of network groups. The key of the map is the name of the individual Network Group.",
+				Computed:            true,
+				NestedObject: schema.NestedAttributeObject{
+					Attributes: map[string]schema.Attribute{
+						"id": schema.StringAttribute{
+							MarkdownDescription: "UUID of the managed Network Group.",
+							Computed:            true,
+						},
+						"description": schema.StringAttribute{
+							MarkdownDescription: "Optional user-created description.",
+							Computed:            true,
+						},
+						"overridable": schema.BoolAttribute{
+							MarkdownDescription: "Indicates whether object values can be overridden.",
+							Computed:            true,
+						},
+						"group_names": schema.SetAttribute{
+							MarkdownDescription: "Set of names (not UUIDs) of child Network Groups. The names must be defined in the same instance of fmc_network_groups resource. This is an auxiliary way to add a child Network Group: the suggested way is to add it inside `objects` by its UUID. Attribute `group_names` is used in managed resource only and always empty in a data-source. Modification of contents of `group_names` is not yet implemented.",
+							ElementType:         types.StringType,
+							Computed:            true,
+						},
+						"objects": schema.SetNestedAttribute{
+							MarkdownDescription: "Set of objects (fmc_network, fmc_host, ...).",
+							Computed:            true,
+							NestedObject: schema.NestedAttributeObject{
+								Attributes: map[string]schema.Attribute{
+									"id": schema.StringAttribute{
+										MarkdownDescription: "UUID of the object (such as fmc_network.this.id, etc.).",
+										Computed:            true,
+									},
+								},
+							},
+						},
+						"literals": schema.SetNestedAttribute{
+							MarkdownDescription: "Set of literal values.",
+							Computed:            true,
+							NestedObject: schema.NestedAttributeObject{
+								Attributes: map[string]schema.Attribute{
+									"value": schema.StringAttribute{
+										MarkdownDescription: "",
+										Computed:            true,
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func (d *NetworkGroupsDataSource) Configure(_ context.Context, req datasource.ConfigureRequest, _ *datasource.ConfigureResponse) {
+	if req.ProviderData == nil {
+		return
+	}
+
+	d.client = req.ProviderData.(*FmcProviderData).Client
+}
+
+// End of section. //template:end model
+
+// Section below is generated&owned by "gen/generator.go". //template:begin read
+
+func (d *NetworkGroupsDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+	var config NetworkGroups
+
+	// Read config
+	diags := req.Config.Get(ctx, &config)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// Set request domain if provided
+	reqMods := [](func(*fmc.Req)){}
+	if !config.Domain.IsNull() && config.Domain.ValueString() != "" {
+		reqMods = append(reqMods, fmc.DomainName(config.Domain.ValueString()))
+	}
+
+	tflog.Debug(ctx, fmt.Sprintf("%s: Beginning Read", config.Id.String()))
+
+	res, err := d.client.Get(config.getPath()+"/"+url.QueryEscape(config.Id.ValueString()), reqMods...)
+	if err != nil {
+		resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Failed to retrieve object, got error: %s", err))
+		return
+	}
+
+	config.fromBody(ctx, res)
+
+	tflog.Debug(ctx, fmt.Sprintf("%s: Read finished successfully", config.Id.ValueString()))
+
+	diags = resp.State.Set(ctx, &config)
+	resp.Diagnostics.Append(diags...)
+}
+
+// End of section. //template:end read
---
+++ internal/provider/model_fmc_network_groups.go
@@ -0,0 +1,174 @@
+// Copyright © 2023 Cisco Systems, Inc. and its affiliates.
+// All rights reserved.
+//
+// Licensed under the Mozilla Public License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://mozilla.org/MPL/2.0/
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: MPL-2.0
+
+package provider
+
+// Section below is generated&owned by "gen/generator.go". //template:begin imports
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/tidwall/gjson"
+	"github.com/tidwall/sjson"
+)
+
+// End of section. //template:end imports
+
+// Section below is generated&owned by "gen/generator.go". //template:begin types
+
+type NetworkGroups struct {
+	Id     types.String                  `tfsdk:"id"`
+	Domain types.String                  `tfsdk:"domain"`
+	Items  map[string]NetworkGroupsItems `tfsdk:"items"`
+}
+
+type NetworkGroupsItems struct {
+	Id          types.String                 `tfsdk:"id"`
+	Description types.String                 `tfsdk:"description"`
+	Overridable types.Bool                   `tfsdk:"overridable"`
+	GroupNames  types.Set                    `tfsdk:"group_names"`
+	Objects     []NetworkGroupsItemsObjects  `tfsdk:"objects"`
+	Literals    []NetworkGroupsItemsLiterals `tfsdk:"literals"`
+}
+
+type NetworkGroupsItemsObjects struct {
+	Id types.String `tfsdk:"id"`
+}
+type NetworkGroupsItemsLiterals struct {
+	Value types.String `tfsdk:"value"`
+}
+
+// End of section. //template:end types
+
+// Section below is generated&owned by "gen/generator.go". //template:begin getPath
+
+func (data NetworkGroups) getPath() string {
+	return "/api/fmc_config/v1/domain/{DOMAIN_UUID}/object/networkgroups"
+}
+
+// End of section. //template:end getPath
+
+// Section below is generated&owned by "gen/generator.go". //template:begin toBody
+
+func (data NetworkGroups) toBody(ctx context.Context, state NetworkGroups) string {
+	body := ""
+	if data.Id.ValueString() != "" {
+		body, _ = sjson.Set(body, "id", data.Id.ValueString())
+	}
+	if len(data.Items) > 0 {
+		body, _ = sjson.Set(body, "items", []interface{}{})
+		for key, item := range data.Items {
+			itemBody, _ := sjson.Set("{}", "name", key)
+			if !item.Id.IsNull() && !item.Id.IsUnknown() {
+				itemBody, _ = sjson.Set(itemBody, "id", item.Id.ValueString())
+			}
+			if !item.Description.IsNull() {
+				itemBody, _ = sjson.Set(itemBody, "description", item.Description.ValueString())
+			}
+			if !item.Overridable.IsNull() {
+				itemBody, _ = sjson.Set(itemBody, "overridable", item.Overridable.ValueBool())
+			}
+			if !item.GroupNames.IsNull() {
+				var values []string
+				item.GroupNames.ElementsAs(ctx, &values, false)
+				itemBody, _ = sjson.Set(itemBody, "group_names", values)
+			}
+			if len(item.Objects) > 0 {
+				itemBody, _ = sjson.Set(itemBody, "objects", []interface{}{})
+				for _, childItem := range item.Objects {
+					itemChildBody := ""
+					if !childItem.Id.IsNull() {
+						itemChildBody, _ = sjson.Set(itemChildBody, "id", childItem.Id.ValueString())
+					}
+					itemChildBody, _ = sjson.Set(itemChildBody, "type", "AnyNonEmptyString")
+					itemBody, _ = sjson.SetRaw(itemBody, "objects.-1", itemChildBody)
+				}
+			}
+			if len(item.Literals) > 0 {
+				itemBody, _ = sjson.Set(itemBody, "literals", []interface{}{})
+				for _, childItem := range item.Literals {
+					itemChildBody := ""
+					if !childItem.Value.IsNull() {
+						itemChildBody, _ = sjson.Set(itemChildBody, "value", childItem.Value.ValueString())
+					}
+					itemChildBody, _ = sjson.Set(itemChildBody, "type", "AnyNonEmptyString")
+					itemBody, _ = sjson.SetRaw(itemBody, "literals.-1", itemChildBody)
+				}
+			}
+			body, _ = sjson.SetRaw(body, "items.-1", itemBody)
+		}
+	}
+	return body
+}
+
+// End of section. //template:end toBody
+
+// Section below is generated&owned by "gen/generator.go". //template:begin fromBody
+
+func (data *NetworkGroups) fromBody(ctx context.Context, res gjson.Result) {
+}
+
+// End of section. //template:end fromBody
+
+// Section below is generated&owned by "gen/generator.go". //template:begin fromBodyPartial
+
+// fromBodyPartial reads values from a gjson.Result into a tfstate model. It ignores null attributes in order to
+// uncouple the provider from the exact values that the backend API might summon to replace nulls. (Such behavior might
+// easily change across versions of the backend API.) For List/Set/Map attributes, the func only updates the
+// "managed" elements, instead of all elements.
+func (data *NetworkGroups) fromBodyPartial(ctx context.Context, res gjson.Result) {
+}
+
+// End of section. //template:end fromBodyPartial
+
+// Section below is generated&owned by "gen/generator.go". //template:begin fromBodyUnknowns
+
+// fromBodyUnknowns updates the Unknown Computed tfstate values from a JSON.
+// Known values are not changed (usual for Computed attributes with UseStateForUnknown or with Default).
+func (data *NetworkGroups) fromBodyUnknowns(ctx context.Context, res gjson.Result) {
+	for i, val := range data.Items {
+		var r gjson.Result
+		res.Get("items").ForEach(
+			func(_, v gjson.Result) bool {
+				if val.Id.IsUnknown() {
+					if v.Get("name").String() == i {
+						r = v
+						return false // break ForEach
+					}
+				} else {
+					if val.Id.ValueString() == v.Get("id").String() {
+						r = v
+						return false // break ForEach
+					}
+				}
+
+				return true
+			},
+		)
+		if data.Items[i].Id.IsUnknown() {
+			v := data.Items[i]
+			if value := r.Get("id"); value.Exists() {
+				v.Id = types.StringValue(value.String())
+			} else {
+				v.Id = types.StringNull()
+			}
+			data.Items[i] = v
+		}
+	}
+}
+
+// End of section. //template:end fromBodyUnknowns
