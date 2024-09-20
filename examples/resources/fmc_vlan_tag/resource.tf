1a25ecf9b30e1db2c6c0a7f50cfe29ffcd3d18a3
Wojciech Spyra <wojciech.spyra@codilime.com>
2024-09-20 13:45:48 +0200

    fmc_vlan_tag resource and data source (#48)

---
+++ examples/resources/fmc_vlan_tag/resource.tf
@@ -0,0 +1,8 @@
+resource "fmc_vlan_tag" "example" {
+  name        = "vlan_tag_1"
+  description = "My TAG id"
+  overridable = true
+  type        = "VlanTag"
+  start_tag   = 12
+  end_tag     = 15
+}
