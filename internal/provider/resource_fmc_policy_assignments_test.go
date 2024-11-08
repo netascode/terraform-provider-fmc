9c70dfb3afb8229b288f38c514fbf2f4a3301a11
Wojciech Spyra <wojciech.spyra@codilime.com>
2024-11-08 15:13:24 +0100

    Add policy assignment resource and data source (#91) applying change requests

--- internal/provider/resource_fmc_policy_assignments_test.go
+++ internal/provider/resource_fmc_policy_assignments_test.go
@@ -30,8 +30,10 @@ import (
 // Section below is generated&owned by "gen/generator.go". //template:begin testAcc
 
 func TestAccFmcPolicyAssignments(t *testing.T) {
+	if os.Getenv("TF_VAR_target_id") == "" {
+		t.Skip("skipping test, set environment variable TF_VAR_target_id")
+	}
 	var checks []resource.TestCheckFunc
-	checks = append(checks, resource.TestCheckResourceAttr("fmc_policy_assignments.test", "targets.0.id", "9862719c-8d5f-11ef-99a6-aef0794da1c1"))
 	checks = append(checks, resource.TestCheckResourceAttr("fmc_policy_assignments.test", "targets.0.type", "DeviceGroup"))
 
 	var steps []resource.TestStep
@@ -67,6 +69,8 @@ resource "fmc_access_control_policy" "example" {
     name                              = "policy-example-test"
     rules                             = []
 }
+
+variable "target_id" { default = null } // tests will set $TF_VAR_target_id
 `
 
 // End of section. //template:end testPrerequisites
@@ -77,7 +81,7 @@ func testAccFmcPolicyAssignmentsConfig_minimum() string {
 	config := `resource "fmc_policy_assignments" "test" {` + "\n"
 	config += `	policy_id = fmc_access_control_policy.example.id` + "\n"
 	config += `	targets = [{` + "\n"
-	config += `		id = "9862719c-8d5f-11ef-99a6-aef0794da1c1"` + "\n"
+	config += `		id = var.target_id` + "\n"
 	config += `		type = "DeviceGroup"` + "\n"
 	config += `	}]` + "\n"
 	config += `}` + "\n"
@@ -92,7 +96,7 @@ func testAccFmcPolicyAssignmentsConfig_all() string {
 	config := `resource "fmc_policy_assignments" "test" {` + "\n"
 	config += `	policy_id = fmc_access_control_policy.example.id` + "\n"
 	config += `	targets = [{` + "\n"
-	config += `		id = "9862719c-8d5f-11ef-99a6-aef0794da1c1"` + "\n"
+	config += `		id = var.target_id` + "\n"
 	config += `		type = "DeviceGroup"` + "\n"
 	config += `	}]` + "\n"
 	config += `}` + "\n"
