981fe8bc8890ae52657f149a55bdaf45ad5213a4
rchrabas <rchrabas@cisco.com>
2024-11-18 11:45:27 +0100

    template updates

--- gen/templates/data_source_test.go
+++ gen/templates/data_source_test.go
@@ -52,6 +52,9 @@ func TestAccDataSourceFmc{{camelCase .Name}}(t *testing.T) {
 	{{- if and (not .WriteOnly) (not .ExcludeTest) (not .Value) (not .TestValue) ( isSet . ) }}
 	{{- $clist := .TfName }}
 	{{- range  .Attributes}}
+	{{- if eq .TfName "id" }}
+	checks = append(checks, resource.TestCheckResourceAttrSet("data.fmc_{{snakeCase $name}}.test", "{{$list}}.{{$map}}.{{$clist}}.0.{{.TfName}}"))
+	{{- end}}
 	{{- if and (not .WriteOnly) (not .ExcludeTest) (not .Value) (not .TestValue) (not (isSet .)) }}
 	checks = append(checks, resource.TestCheckResourceAttr("data.fmc_{{snakeCase $name}}.test", "{{$list}}.{{$map}}.{{$clist}}.0.{{.TfName}}", "{{.Example}}"))
 	{{- end}}
