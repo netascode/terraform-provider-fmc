b8beb9130983326ab614f4bb1909e5ffe62ec544
rchrabas <rchrabas@cisco.com>
2024-12-10 11:30:25 +0100

    go generate

--- internal/provider/resource_fmc_device_subinterface_test.go
+++ internal/provider/resource_fmc_device_subinterface_test.go
@@ -30,8 +30,8 @@ import (
 // Section below is generated&owned by "gen/generator.go". //template:begin testAcc
 
 func TestAccFmcDeviceSubinterface(t *testing.T) {
-	if os.Getenv("TF_VAR_device_id") == "" && os.Getenv("TF_VAR_interface_name") == "" {
-		t.Skip("skipping test, set environment variable TF_VAR_device_id or TF_VAR_interface_name")
+	if os.Getenv("TF_VAR_device_id") == "" || os.Getenv("TF_VAR_interface_name") == "" {
+		t.Skip("skipping test, set environment variable TF_VAR_device_id and TF_VAR_interface_name")
 	}
 	var checks []resource.TestCheckFunc
 	checks = append(checks, resource.TestCheckResourceAttrSet("fmc_device_subinterface.test", "type"))
