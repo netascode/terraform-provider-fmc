bf4843ed4541ea141fee300ddc59a9ed1b38e12d
mmaciejc@cisco.com <mmaciejc@cisco.com>
2024-12-18 10:17:03 +0100

    initial files for fmc_device_cluster support

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -294,6 +294,7 @@ func (p *FmcProvider) Resources(ctx context.Context) []func() resource.Resource 
 	return []func() resource.Resource{
 		NewAccessControlPolicyResource,
 		NewDeviceResource,
+		NewDeviceClusterResource,
 		NewDeviceEtherChannelInterfaceResource,
 		NewDeviceIPv4StaticRouteResource,
 		NewDeviceIPv6StaticRouteResource,
@@ -346,6 +347,7 @@ func (p *FmcProvider) DataSources(ctx context.Context) []func() datasource.DataS
 	return []func() datasource.DataSource{
 		NewAccessControlPolicyDataSource,
 		NewDeviceDataSource,
+		NewDeviceClusterDataSource,
 		NewDeviceEtherChannelInterfaceDataSource,
 		NewDeviceIPv4StaticRouteDataSource,
 		NewDeviceIPv6StaticRouteDataSource,
