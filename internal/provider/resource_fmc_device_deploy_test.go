0bacc679bfbded4d7e107d7f7cb0b8106b561483
rchrabas <rchrabas@cisco.com>
2025-01-22 10:10:43 +0100

    device_deploy: device_list > device_id_list

--- internal/provider/resource_fmc_device_deploy_test.go
+++ internal/provider/resource_fmc_device_deploy_test.go
@@ -67,7 +67,7 @@ variable "device_id" { default = null } // tests will set $TF_VAR_device_id
 
 func testAccFmcDeviceDeployConfig_minimum() string {
 	config := `resource "fmc_device_deploy" "test" {` + "\n"
-	config += `	device_list = [var.device_id]` + "\n"
+	config += `	device_id_list = [var.device_id]` + "\n"
 	config += `}` + "\n"
 	return config
 }
@@ -78,7 +78,7 @@ func testAccFmcDeviceDeployConfig_minimum() string {
 
 func testAccFmcDeviceDeployConfig_all() string {
 	config := `resource "fmc_device_deploy" "test" {` + "\n"
-	config += `	device_list = [var.device_id]` + "\n"
+	config += `	device_id_list = [var.device_id]` + "\n"
 	config += `}` + "\n"
 	return config
 }
